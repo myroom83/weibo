a98a2b521fc782fa4ca533a5f9580a0a3c7c1bc5
jeff sun <jeff.myroom83@gmail.com>
2019-09-04 00:45:48 +0800

    用户注册界面

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
@@ -8,7 +10,7 @@
 
   <body>
 
-  @include('layouts._header')
+    @include('layouts._header')
 
     <div class="container">
       <div class="offset-md-1 col-md-10">
